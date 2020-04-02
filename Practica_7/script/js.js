5ae1689f2203ff46db9c399fd15ad8b6e866a64f
Luis Enrique <luisechvz@gmail.com>
2020-04-02 11:41:40 -0600

    final 7

--- Practica_7/script/js.js
+++ Practica_7/script/js.js
@@ -1,7 +1,7 @@
 let btnSend = document.getElementById('submit');
 var soloNumbers = new RegExp("^[0-9]+$");
 var soloNumbersFloat = new RegExp("^[0-9,'.']+$");
-var soloCharacters = new RegExp("^[a-z,A-Z,' ',á,é,í,ó,ú,Á,É,Í,Ó,Ú]+$");
+var soloCharacters = new RegExp("^[a-z,A-Z,' ',á,é,í,ó,ú,Á,É,Í,Ó,Ú,'.']+$");
 
 btnSend.addEventListener('click', () => {
     /*datos personales*/
@@ -68,6 +68,21 @@ btnSend.addEventListener('click', () => {
 
     /*VALIDACION*/
     if (soloCharacters.test(nombre, apPaterno, apMaterno, calle) && soloNumbers.test(numeroCasa, codigoPostal) && soloNumbersFloat.test(promedio)) {
+        alert("Impresion final de datos CORRECTOS.")
+        imprimir();
+    } else {
+        if (soloCharacters.test(nombre)) { console.log("ok") } else { alert("El nombre esta mal o vacio. (No use numeros ni simbolos especiales)") }
+        if (soloCharacters.test(apPaterno)) { console.log("ok") } else { alert("El apellido paterno esta mal o vacio. (No use numeros ni simbolos especiales)") }
+        if (soloCharacters.test(apMaterno)) { console.log("ok") } else { alert("El apellido materno esta mal o vacio. (No use numeros ni simbolos especiales)") }
+        if (soloCharacters.test(calle)) { console.log("ok") } else { alert("La calle esta mal o vacio. (No use numeros ni simbolos especiales)") }
+        if (soloNumbersFloat.test(promedio)) { console.log("ok") } else { alert("El promedio esta mal o vacio. (No use letras, espacios o simbolos especiales (Excepto el punto '.'))") }
+        if (soloNumbers.test(numeroCasa)) { console.log("ok") } else { alert("El numero de casa esta mal o vacio. (No use letras, espacios o simbolos especiales)") }
+        if (soloNumbers.test(codigoPostal)) { console.log("ok") } else { alert("El C.P. de casa esta mal o vacio. (No use letras, espacios o simbolos especiales)") }
+        alert("Impresion de datos. // Se recomienda corregir valores indicados.");
+        imprimir();
+    }
+
+    function imprimir(){
         alert("/////Datos personales. \r"
             + "Nombre = " + nombre + "\r"
             + "Apellido paterno = " + apPaterno + "\r"
@@ -96,16 +111,6 @@ btnSend.addEventListener('click', () => {
             + "/////Informacion de la cuenta. \r"
             + "Nombre del usuario = " + user + "\r"
             + "Password = " + password + "\r");
-        console.log("Dato");
-    } else {
-        if (soloCharacters.test(nombre)) { console.log("ok") } else { alert("El nombre esta mal o vacio. (No use numeros ni simbolos especiales)") }
-        if (soloCharacters.test(apPaterno)) { console.log("ok") } else { alert("El apellido paterno esta mal o vacio. (No use numeros ni simbolos especiales)") }
-        if (soloCharacters.test(apMaterno)) { console.log("ok") } else { alert("El apellido materno esta mal o vacio. (No use numeros ni simbolos especiales)") }
-        if (soloCharacters.test(calle)) { console.log("ok") } else { alert("La calle esta mal o vacio. (No use numeros ni simbolos especiales)") }
-        if (soloNumbersFloat.test(promedio)) { console.log("ok") } else { alert("El promedio esta mal o vacio. (No use letras, espacios o simbolos especiales (Excepto el punto '.'))") }
-        if (soloNumbers.test(numeroCasa)) { console.log("ok") } else { alert("El numero de casa esta mal o vacio. (No use letras, espacios o simbolos especiales)") }
-        if (soloNumbers.test(codigoPostal)) { console.log("ok") } else { alert("El C.P. de casa esta mal o vacio. (No use letras, espacios o simbolos especiales)") }
-
     }
 
 
