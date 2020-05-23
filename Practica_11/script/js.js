59af13253bb1c799cf724e00504d967f2af7db3b
Luis Enrique <luisechvz@gmail.com>
2020-05-22 23:32:21 -0500

    Agregue la práctica 11

--- Practica_11/script/js.js
+++ Practica_11/script/js.js
@@ -1,46 +1,50 @@
 class Stack {
   constructor() {
-    this.data = [];
-    this.top = 0;
+    this.datosDePila = [];
+    this.topeDeLaPila = 0;
   }
+
+  //Para la insercion
   push(element) {
-    this.data[this.top] = element;
-    this.top = this.top + 1;
+    this.datosDePila[this.topeDeLaPila] = element;
+    this.topeDeLaPila = this.topeDeLaPila + 1;
   }
+
+  //Para la longitud
   length() {
-    return this.top;
+    return this.topeDeLaPila;
   }
+
+  //Para saber el topeDeLaPilae de la pila
   peek() {
-    return this.data[this.top - 1];
+    return this.datosDePila[this.topeDeLaPila - 1];
   }
+
+  //Nos ayuda a saber si vacia
   isEmpty() {
-    return this.top === 0;
+    return this.topeDeLaPila === 0;
   }
+
+  //saca la ultima celda
   pop() {
     if (this.isEmpty() === false) {
-      this.top = this.top - 1;
-      return this.data.pop(); // removes the last element
+      this.topeDeLaPila = this.topeDeLaPila - 1;
+      return this.datosDePila.pop();
     }
   }
+
+  //para visualizacion y pruebas
   print() {
-    var top = this.top - 1; // because top points to index where new    element to be inserted
-    while (top >= 0) { // print upto 0th index
-      console.log(this.data[top]);
-      top--;
+    var topeDeLaPila = this.topeDeLaPila - 1;
+    while (topeDeLaPila >= 0) {
+      console.log(this.datosDePila[topeDeLaPila]);
+      topeDeLaPila--;
     }
   }
 }
 
 //Pruebas de stack
 const stack = new Stack();
-/*console.log(stack.length());
-console.log(stack.push('john'));
-console.log(stack.push('The rock'));
-console.log(stack.length());
-console.log(stack.peek());
-console.log(stack.pop());
-console.log(stack.peek());*/
-
 
 //VALORES DEL Form
 var btn1, btn2, btn3;
@@ -49,7 +53,7 @@ btn2 = document.getElementById('btn2');
 btn3 = document.getElementById('btn3');
 
 //Ingresamos los valores al stack
-var valorAdd;
+var valorAdd; /*Valor recibido por nuestro input*/
 btn1.onclick = function () {
   valorAdd = document.getElementById('value').value;
   if (valorAdd == '') {
@@ -57,76 +61,109 @@ btn1.onclick = function () {
   } else {
     stack.push(valorAdd);
     stack.print();
-    alert("El valor ingresado es " + valorAdd);
-
-    meterCelda(valorAdd);
+    meterCelda(valorAdd); /*Insercion de elementos*/
+    insertsCode();
+    insertOfCode[3].classList.add("hidder");
+    operationsPush(valorAdd);
     return valorAdd;
   }
 }
 
 //Meter valor en la celda
 var boxStack = document.getElementById('boxStack');
+var pointersDesign = document.getElementsByClassName('pointerGris');
+var celdasReset = document.getElementsByClassName("celda");
+var textos = document.getElementsByClassName("textoCelda");
 function meterCelda(valorAdd) {
-  if (stack.length() == 1) {
-    var newDiv = document.createElement("div");
-    newDiv.classList.add("celda");
 
-    var newText = document.createElement("p");
-    newText.classList.add("textoCelda", "first");
-    newText.innerHTML = valorAdd;
+  var newDiv = document.createElement("div");
+  newDiv.classList.add("celda");
 
-    newDiv.appendChild(newText);
-    boxStack.appendChild(newDiv);
-  } else {
-    var newDiv = document.createElement("div");
-    newDiv.classList.add("celda");
+  var newText = document.createElement("p");
+  newText.classList.add("textoCelda");
+  newText.innerHTML = valorAdd;
+  newDiv.appendChild(newText);
+  boxStack.appendChild(newDiv);
+  newDiv.classList.add("animacionCelda");
 
-    var newText = document.createElement("p");
-    newText.classList.add("textoCelda");
-    newText.innerHTML = valorAdd;
+  pointersDesign[0].classList.remove('hidder');
+  pointersDesign[1].classList.remove('hidder');
 
-    newDiv.appendChild(newText);
-    boxStack.appendChild(newDiv);
-  }
-
-  var textoCeldaFix = document.getElementsByClassName('textoCelda');
-  textoCeldaFix[textoCeldaFix.length - 1].classList.add("first");
-
-
-  //Arregla el fallo del posicionamiento del texto
-  for (var k = 3; k <= stack.length(); k++) {
-    var contador = 1;
-    if (stack.length() >= 3) {
-      textoCeldaFix[stack.length() - 2].classList.remove("first");
-    }
-  }
 }
 
+
 //Borrar valor
+var detectaBorrado = 0;
 btn2.onclick = function () {
   if (stack.length() == 0) {
     alert("No hay valores en tu pila");
   } else {
     stack.pop();
     stack.print();
-    alert("El valor ha sido borrado");
+    salidaCelda();
+    insertsCode();
+    insertOfCode[1].classList.add("hidder");
+    insertOfCode[2].classList.add("hidder");
+    insertOfCode[3].classList.remove("hidder");
+    operationsPop();
   }
+}
+
+var final = document.getElementById('pointerUp');
+var init = document.getElementById('pointerDown');
+
+function salidaCelda(){
+  celdasReset[celdasReset.length - 1].classList.add('salidaCelda');
+  var timerMio = setTimeout(borrarCelda, 1000);
+  
+}
+
+function borrarCelda() {
+  //console.log(celdasReset);
+  boxStack.removeChild(celdasReset[celdasReset.length - 1]);
+  if (stack.length() == 0) {
+    final.classList.add('hidder');
+    init.classList.add('hidder');
+  } else {
+    final.classList.remove('hidder');
+    init.classList.remove('hidder');
+  }
+  return celdasReset;
 }
 
 //reset all STACK
-var celdasReset = document.getElementsByClassName("celda");
 btn3.onclick = function () {
-  var size = stack.length();
-  if(size == 0){
-    alert("Aun no hay nada en la pila")
-  }else{
-    for (var i = 0; i < size; i++) {
-      stack.pop();
-      celdasReset[i].classList.add("hidder");
-      console.log(stack.length())
-    }
     location.reload(true);
+}
+
+/*Codigo sections*/
+
+var insertOfCode = document.getElementsByClassName("inserts");
+
+function insertsCode(){
+  if( stack.length() > 0){
+    insertOfCode[2].classList.remove("hidder");
+    insertOfCode[1].classList.remove("hidder");
+  }else if( stack.length() == 0){
+    insertOfCode[2].classList.add("hidder");
+    insertOfCode[1].classList.add("hidder");
+    insertOfCode[3].classList.add("hidder");
   }
 }
 
+var operationsBox = document.getElementsByClassName("operations");
+var operationSingle;
+function operationsPush(valorAdd){
+  operationSingle = document.createElement("p");
+  operationSingle.classList.add("oranger");
+  operationsBox[0].appendChild(operationSingle);
+  operationSingle.innerHTML = "push(nodo," + valorAdd + ")";
+}
+
+function operationsPop(){
+  operationSingle = document.createElement("p");
+  operationSingle.classList.add("oranger");
+  operationsBox[0].appendChild(operationSingle);
+  operationSingle.innerHTML = "pop(nodo)";
+}
 
